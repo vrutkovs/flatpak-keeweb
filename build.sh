1387732697ea6564c3e94391a3ecb3f342909469
Vadim Rutkovsky <vrutkovs@redhat.com>
2017-03-24 01:06:28 +0100

    Use global electron app

--- build.sh
+++ build.sh
@@ -4,14 +4,6 @@ set -eux
 
 rm -rf app
 
-pushd electron-flatpak-base-app
-    make
-    flatpak build-update-repo --prune --prune-depth=20 ./repo
-    flatpak --user remote-add --no-gpg-verify local-electron ./repo || true
-    flatpak --user -v install local-electron io.atom.electron.BaseApp || true
-    flatpak update --user io.atom.electron.BaseApp
-popd
-
 flatpak-builder --ccache --require-changes --repo=repo --subject="Nightly build of Keeweb, `date`" app info.keeweb.App.json
 
 flatpak build-update-repo --prune --prune-depth=20 repo
